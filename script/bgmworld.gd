99c421d00e57b044dafdc62fd653f61e31ab5a86
Ika Puspita <ikapuspita854@gmail.com>
2025-01-13 19:59:27 +0700

    feat: tambah musik pada game

---
+++ script/bgmworld.gd
@@ -0,0 +1,16 @@
+extends AudioStreamPlayer2D
+
+func _ready():
+	var music = load("res://assets/sound/BGM-World.mp3")
+	
+	# Aktifkan looping pada stream
+	music.loop = true
+	
+	# Set stream ke AudioStreamPlayer2D
+	stream = music
+	
+	# Atur volume (dalam dB, misalnya -10 untuk volume lebih rendah)
+	volume_db = -10
+	
+	# Mulai memutar musik
+	play()
